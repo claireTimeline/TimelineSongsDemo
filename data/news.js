151b121eb709ab8ddd8520035a9c3c7658d0c2f1
claireTimeline <claireyhannon@gmail.com>
2026-03-23 21:35:31 +0000

    Add news data for upcoming events

---
+++ data/news.js
@@ -0,0 +1,10 @@
+const news = [
+  {
+    date: "March 2026",
+    text: "Tickets now on sale for our Spring Concert!"
+  },
+  {
+    date: "February 2026",
+    text: "Welcome to our new members 🎶"
+  }
+];
